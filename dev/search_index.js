f81ca99c4c2d201ce2a476ab6e744a1004a8c2dd
Documenter.jl <documenter@juliadocs.github.io>
2023-09-30 08:55:07 +0000

    build based on 5996f90

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HDF5Indexers","category":"page"},{"location":"#HDF5Indexer","page":"Home","title":"HDF5Indexer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HDF5Indexers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HDF5Indexers]","category":"page"}]
+}
